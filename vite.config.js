e578f548edbad82bc52ddc0df721282a818f2d58
TiGz <ajchesney@gmail.com>
2025-03-31 16:00:48 +0100

    trying to fix github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // This will be the base path for GitHub Pages
+  base: '/eve-web-admin/', // This will be the base path for GitHub Pages
   plugins: [vue()],
   build: {
     outDir: 'dist',
